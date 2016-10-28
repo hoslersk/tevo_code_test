fa0a3d18472299cbe2a1cbd00c10f91d6b7cbfc6
Schyler Hosler <schylerhosler@gmail.com>
2016-10-28 16:00:39 -0400

    All output correct and running from run.rb file

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,14 @@
 require './lib/text_parser'
+require './lib/text_displayer'
 
 test_parser = TextParser.new
 
 test_parser.parse_comma_txt('./input_files/comma.txt')
 test_parser.parse_pipe_txt('./input_files/pipe.txt')
 test_parser.parse_space_txt('./input_files/space.txt')
+
+test_displayer = TextDisplayer.new
+
+test_displayer.output1(test_parser)
+test_displayer.output2(test_parser)
+test_displayer.output3(test_parser)
--- lib/text_displayer.rb
+++ lib/text_displayer.rb
@@ -5,51 +5,35 @@ class TextDisplayer
   def output_display(consumer_list, current_method)
     puts "Output #{current_method.to_s[-1]}:" # interpolates the output number from method name
     consumer_list.each do |details|
-        puts "#{details[:last_name]} #{details[:first_name]} #{details[:gender]} #{details[:dob]} #{details[:favorite_color]}" # interpolates all necessary data in presorted order
+      puts "#{details[:last_name]} #{details[:first_name]} #{details[:gender]} #{details[:dob]} #{details[:favorite_color]} " # interpolates all necessary data in presorted order
     end
+    puts "\n" # adds spacing found in output sample
+  end
+
+  def output1(text_parser)
+    consumer_list = text_parser.consumer_record[:consumers]
+    consumer_list.sort_by! do |keys|
+      [ keys[:gender], keys[:last_name] ] # sorts by gender, then by last name
+    end
+    output_display(consumer_list, __method__) # passing current method as argument for dynamic use of integer
   end
 
   def output2(text_parser)
     consumer_list = text_parser.consumer_record[:consumers]
-    consumer_list.sort_by! do |key|
-      key[:dob][-4..-1]
-      # if key[:dob].split("/")[0].to_i < 10 && key[:dob].split("/")[1].to_i < 10
-      #   "0" + key[:dob].split("/")[0] + "0" + key[:dob].split("/")[1] + key[:dob].split("/")[2]
-      # elsif key[:dob].split("/")[0].to_i < 10 && key[:dob].split("/")[1].to_i > 9
-      #   "0"+(key[:dob]).gsub("/", "")
-      # elsif key[:dob].split("/")[0].to_i > 9 && key[:dob].split("/")[1].to_i < 10
-      #   key[:dob].split("/")[0] + "0" + (key[:dob].split("/")[1]) + key[:dob].split("/")[2]
-      # else
-      #   key[:dob].gsub("/", "")
-      # end
+    consumer_list.sort_by! do |keys|
+      [ keys[:dob].split("/")[2], keys[:dob].split("/")[1], keys[:dob].split("/")[0], keys[:last_name] ]
+      # sorts in order of birth year, month, day, and then last name (for examples w/ repeat dob)
     end
     output_display(consumer_list, __method__) # passing current method as argument for dynamic use of integer
   end
 
   def output3(text_parser)
     consumer_list = text_parser.consumer_record[:consumers] # pulling data from text parser
-    consumer_list.sort_by! do |key|
-      key[:last_name] # sorting by last name
+    consumer_list.sort_by! do |keys|
+      keys[:last_name] # sorting by last name
     end
     consumer_list.reverse! # reversing for descending order
     output_display(consumer_list, __method__) # passing current method as argument for dynamic use of integer
   end
 
 end
-
-test_displayer = TextDisplayer.new
-
-test_parser = TextParser.new
-
-test_parser.parse_comma_txt('./input_files/comma.txt')
-test_parser.parse_pipe_txt('./input_files/pipe.txt')
-test_parser.parse_space_txt('./input_files/space.txt')
-
-test_displayer.output2(test_parser)
-test_displayer.output3(test_parser)
-
-# output 1:
-# sorted by gender (females first/alphabetical) and then last name, ascending
-
-# output 2:
-# sorted by birthdate, ascending
