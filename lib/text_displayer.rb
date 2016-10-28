9133bcd7357fed2f62ce2a3d1b4f4283bf6ff993
Schyler Hosler <schylerhosler@gmail.com>
2016-10-28 12:52:04 -0400

    Output 3 for last name, descending now successful

--- lib/text_displayer.rb
+++ lib/text_displayer.rb
@@ -2,8 +2,22 @@ require_relative './text_parser'
 
 class TextDisplayer
 
-  def output1(text_parser)
+  def output3(text_parser)
     # text_parser.consumer_record
+    consumer_list = text_parser.consumer_record[:consumers]
+    last_name_array = []
+    consumer_list.each do |details, consumer|
+      last_name_array.push(consumer[:last_name])
+    end
+    last_name_array.sort!.reverse!
+    puts "Output 3:"
+    last_name_array.each do |last_name|
+      consumer_list.each do |details, consumer|
+        if consumer[:last_name] == last_name
+          puts "#{consumer[:last_name]} #{consumer[:first_name]} #{consumer[:gender]} #{consumer[:dob]} #{consumer[:favorite_color]}"
+        end
+      end
+    end
   end
 
 end
@@ -16,7 +30,7 @@ test_parser.parse_comma_txt('./input_files/comma.txt')
 test_parser.parse_pipe_txt('./input_files/pipe.txt')
 test_parser.parse_space_txt('./input_files/space.txt')
 
-test_displayer.output1(test_parser)
+test_displayer.output3(test_parser)
 
 # output 1:
 # sorted by gender (females first/alphabetical) and then last name, ascending
