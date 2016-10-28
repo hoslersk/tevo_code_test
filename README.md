db7f44463d33766d3f4d478369e999f7d19dfed3
Schyler Hosler <schylerhosler@gmail.com>
2016-10-28 16:09:22 -0400

    Initial creation of README.md file

---
+++ README.md
@@ -0,0 +1,3 @@
+### TEvo Code Challenge For .txt File Parsing, Data Collecting, and Reformatting
+
+Once cloned down, run ```ruby bin/run.rb``` within terminal to receive output results.
--- lib/text_displayer.rb
+++ lib/text_displayer.rb
@@ -5,7 +5,7 @@ class TextDisplayer
   def output_display(consumer_list, current_method)
     puts "Output #{current_method.to_s[-1]}:" # interpolates the output number from method name
     consumer_list.each do |details|
-      puts "#{details[:last_name]} #{details[:first_name]} #{details[:gender]} #{details[:dob]} #{details[:favorite_color]} " # interpolates all necessary data in presorted order
+      puts "#{details[:last_name]} #{details[:first_name]} #{details[:gender]} #{details[:dob]} #{details[:favorite_color]} "
     end
     puts "\n" # adds spacing found in output sample
   end
@@ -15,7 +15,7 @@ class TextDisplayer
     consumer_list.sort_by! do |keys|
       [ keys[:gender], keys[:last_name] ] # sorts by gender, then by last name
     end
-    output_display(consumer_list, __method__) # passing current method as argument for dynamic use of integer
+    output_display(consumer_list, __method__) # passing current method as argument for more dynamic use
   end
 
   def output2(text_parser)
@@ -24,7 +24,7 @@ class TextDisplayer
       [ keys[:dob].split("/")[2], keys[:dob].split("/")[1], keys[:dob].split("/")[0], keys[:last_name] ]
       # sorts in order of birth year, month, day, and then last name (for examples w/ repeat dob)
     end
-    output_display(consumer_list, __method__) # passing current method as argument for dynamic use of integer
+    output_display(consumer_list, __method__) # passing current method as argument for more dynamic use
   end
 
   def output3(text_parser)
@@ -33,7 +33,7 @@ class TextDisplayer
       keys[:last_name] # sorting by last name
     end
     consumer_list.reverse! # reversing for descending order
-    output_display(consumer_list, __method__) # passing current method as argument for dynamic use of integer
+    output_display(consumer_list, __method__) # passing current method as argument for more dynamic use
   end
 
 end
