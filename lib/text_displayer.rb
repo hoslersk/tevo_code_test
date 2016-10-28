ac30b1eda3c14fb15027cda3910187d9b7b75f83
Schyler Hosler <schylerhosler@gmail.com>
2016-10-28 15:28:15 -0400

    This update will enable 2 of 3 outputs... Currently successful with output2 and output 3 methods.

--- lib/text_displayer.rb
+++ lib/text_displayer.rb
@@ -2,39 +2,37 @@ require_relative './text_parser'
 
 class TextDisplayer
 
+  def output_display(consumer_list, current_method)
+    puts "Output #{current_method.to_s[-1]}:" # interpolates the output number from method name
+    consumer_list.each do |details|
+        puts "#{details[:last_name]} #{details[:first_name]} #{details[:gender]} #{details[:dob]} #{details[:favorite_color]}" # interpolates all necessary data in presorted order
+    end
+  end
+
   def output2(text_parser)
     consumer_list = text_parser.consumer_record[:consumers]
-    dob_array = []
-    consumer_list.each do |details, consumer|
-      dob_array.push(consumer[:dob])
+    consumer_list.sort_by! do |key|
+      key[:dob][-4..-1]
+      # if key[:dob].split("/")[0].to_i < 10 && key[:dob].split("/")[1].to_i < 10
+      #   "0" + key[:dob].split("/")[0] + "0" + key[:dob].split("/")[1] + key[:dob].split("/")[2]
+      # elsif key[:dob].split("/")[0].to_i < 10 && key[:dob].split("/")[1].to_i > 9
+      #   "0"+(key[:dob]).gsub("/", "")
+      # elsif key[:dob].split("/")[0].to_i > 9 && key[:dob].split("/")[1].to_i < 10
+      #   key[:dob].split("/")[0] + "0" + (key[:dob].split("/")[1]) + key[:dob].split("/")[2]
+      # else
+      #   key[:dob].gsub("/", "")
+      # end
     end
-    # puts dob_array.sort_by! {|date| date[-4..-1]}
-    puts "Output 2:"
-    dob_array.sort_by! do |date|
-      if date.split("/")[0].to_i < 10 && date.split("/")[1].to_i < 10
-        "0"+(date)
-        "0"+(date.split("/")[1])
-      elsif date.split("/")[0].to_i < 10 && date.split("/")[1].to_i > 9
-        "0"+(date).gsub("/", "")
-      elsif date.split("/")[0].to_i > 9 && date.split("/")[1].to_i < 10
-        "0"+(date.split("/")[1])
-      else
-        date.gsub("/", "")
-      end
-    end
-
+    output_display(consumer_list, __method__) # passing current method as argument for dynamic use of integer
   end
 
   def output3(text_parser)
-    consumer_list = text_parser.consumer_record[:consumers]
+    consumer_list = text_parser.consumer_record[:consumers] # pulling data from text parser
     consumer_list.sort_by! do |key|
-      key[:last_name]
+      key[:last_name] # sorting by last name
     end
-    consumer_list.reverse!
-    puts "Output 3:" # submits heading prior to iteration
-      consumer_list.each do |details|
-          puts "#{details[:last_name]} #{details[:first_name]} #{details[:gender]} #{details[:dob]} #{details[:favorite_color]}"
-      end
+    consumer_list.reverse! # reversing for descending order
+    output_display(consumer_list, __method__) # passing current method as argument for dynamic use of integer
   end
 
 end
@@ -47,7 +45,7 @@ test_parser.parse_comma_txt('./input_files/comma.txt')
 test_parser.parse_pipe_txt('./input_files/pipe.txt')
 test_parser.parse_space_txt('./input_files/space.txt')
 
-# test_displayer.output2(test_parser)
+test_displayer.output2(test_parser)
 test_displayer.output3(test_parser)
 
 # output 1:
