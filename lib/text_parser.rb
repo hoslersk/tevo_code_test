65f0bd59999e78e6f238c1d66d98c76a2d7cabb8
Schyler Hosler <schylerhosler@gmail.com>
2016-10-28 17:56:54 -0400

    This update includes 3 successful tests for TextParser data reformatting

--- lib/text_parser.rb
+++ lib/text_parser.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class TextParser
 
   attr_reader :consumer_record
--- spec/text_parser_spec.rb
+++ spec/text_parser_spec.rb
@@ -1,6 +1,6 @@
 require './lib/text_parser'
 
-describe "Text Parser collects data from .txt files" do
+describe "Text Parser collects and reformats data from .txt files" do
 
   let(:test_parser) { TextParser.new }
 
@@ -12,10 +12,12 @@ describe "Text Parser collects data from .txt files" do
   it "Removes pipes from text" do
     pipe_txt = './spec/input_files_spec/pipe_spec.txt'
     test_parser.parse_pipe_txt(pipe_txt)
+    expect(test_parser.consumer_record).to eq({:consumers=>[{:last_name=>"Smith", :first_name=>"Steve", :gender=>"Male", :favorite_color=>"Red", :dob=>"3/3/1985"}, {:last_name=>"Bonk", :first_name=>"Radek", :gender=>"Male", :favorite_color=>"Green", :dob=>"6/3/1975"}, {:last_name=>"Bouillon", :first_name=>"Francis", :gender=>"Male", :favorite_color=>"Blue", :dob=>"6/3/1975"}]})
   end
   it "Removes spaces from text" do
     space_txt = './spec/input_files_spec/space_spec.txt'
     test_parser.parse_space_txt(space_txt)
+    expect(test_parser.consumer_record).to eq({:consumers=>[{:last_name=>"Kournikova", :first_name=>"Anna", :gender=>"Female", :favorite_color=>"Red", :dob=>"6/3/1975"}, {:last_name=>"Hingis", :first_name=>"Martina", :gender=>"Female", :favorite_color=>"Green", :dob=>"4/2/1979"}, {:last_name=>"Seles", :first_name=>"Monica", :gender=>"Female", :favorite_color=>"Black", :dob=>"12/2/1973"}]})
   end
 
 end
